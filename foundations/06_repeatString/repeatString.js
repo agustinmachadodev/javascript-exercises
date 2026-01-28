e5aefd18dac79593118d4425378a3c68f3a8505c
Agustin Machado <agustinmachado.dev@gmail.com>
2026-01-28 14:40:50 -0300

    Complete 06_repeatString Exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let count = '';
 
+    if(num < 0) {
+        return 'ERROR';
+    
+    }  else if(num > 0) {
+        for(let i = 0; i < num; i++) {
+            count += string
+        }
+    }
+    return(count);
 };
 
 // Do not edit below this line
