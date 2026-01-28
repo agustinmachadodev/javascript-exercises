33218a70da784db93f961dcb23bee23cacd3668b
Agustin Machado <agustinmachado.dev@gmail.com>
2026-01-28 17:07:30 -0300

    Complete 07_reverseString Exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverse = ''
+    for(let i = text.length - 1; i >= 0; i--) {
+        reverse += text[i]
+    }
+    return reverse;
 };
 
 // Do not edit below this line
