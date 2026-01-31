e3e95b9030b9a283e1077757172b5f18333c3478
Agustin Machado <agustinmachado.dev@gmail.com>
2026-01-30 21:09:56 -0300

    Complete 08_removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...arg) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(!arg.includes(arr[i])) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
