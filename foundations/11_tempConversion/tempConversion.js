3889d22a07ce4112e106a07a82b530eec51846cc
Agustin Machado <agustinmachado.dev@gmail.com>
2026-02-02 00:18:35 -0300

    Complete 11_tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let conversion = (num - 32) / 1.8
+  return parseFloat(conversion.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
-};
+
+const convertToFahrenheit = function(num){
+  let conversion = (num * 1.8) + 32
+  return parseFloat(conversion.toFixed(1))
+} 
 
 // Do not edit below this line
 module.exports = {
