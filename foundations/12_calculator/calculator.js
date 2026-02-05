827eaca59c2b96dad5e07618dd43b5038df54df3
Agustin Machado <agustinmachado.dev@gmail.com>
2026-02-04 23:26:15 -0300

    Complete 10_calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let total = 0;
+    for(let num of arr) {
+      total += num
+    }
+
+    return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+    let total = 1;
 
+    for(let i = 0; i < arr.length; i++) {
+        total *= arr[i];
+    }
+
+    return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let fact = 1;
+
+    for(let i = a; i > 1 ; i-- ) {
+      fact *= i;
+    }
+
+    return fact;
 };
 
 // Do not edit below this line
